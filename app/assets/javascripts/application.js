08e931114414e13bf08fc610b2791ce6c6023888
YOUR NAME <pong_thailand@hotmail.com>
2017-09-10 07:34:12 +0700

    working demo

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,6 +20,37 @@
 //= require materialize-sprockets
 //= require materialize/extras/nouislider
 
+
+
+var range = document.getElementById('slider');
+
+range.style.height = '400px';
+range.style.margin = '0 auto 30px';
+
+noUiSlider.create(slider, {
+	start: [ 1450, 2050, 2350, 3000 ], // 4 handles, starting at...
+	margin: 300, // Handles must be at least 300 apart
+	limit: 600, // ... but no more than 600
+	connect: true, // Display a colored bar between the handles
+	direction: 'rtl', // Put '0' at the bottom of the slider
+	orientation: 'vertical', // Orient the slider vertically
+	behaviour: 'tap-drag', // Move handle on tap, bar is draggable
+	step: 150,
+	tooltips: true,
+	format: wNumb({
+		decimals: 0
+	}),
+	range: {
+		'min': 1300,
+		'max': 3250
+	},
+	pips: { // Show a scale with the slider
+		mode: 'steps',
+		stepped: true,
+		density: 4
+	}
+});
+
 handler = Gmaps.build('Google');
 handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
   markers = handler.addMarkers([
@@ -38,17 +69,7 @@ handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
   handler.fitMapToBounds();
 });
 
-var slider = document.getElementById('test-slider');
-noUiSlider.create(slider, {
- start: [20, 80],
- connect: true,
- step: 1,
- orientation: 'horizontal', // 'horizontal' or 'vertical'
- range: {
-   'min': 0,
-   'max': 100
- },
- format: wNumb({
-   decimals: 0
- })
-});
+
+setTimeout(function(){
+    document.getElementById('spinner').className = 'spun';
+}, 5000);
